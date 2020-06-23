2f82294f89f4ce97b5318b941fb2f9b521a276aa
Maurikzio <m.morocho1196@gmail.com>
2020-06-23 21:32:39 +0300

    [ADD] routes and home page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,60 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
 import './App.css';
+import Accordion from './components/accordion/Accordion';
+import Home from './components/home/Home';
+
+const App2 = () =>{
+  return <h2>App2</h2>
+}
+
+const routes = [
+  {
+    path: "/",
+    exact: true,
+    name: "Home",
+    component: <Home/>
+  },
+  {
+    path: "/accordion",
+    name: "Accordion static data",
+    component: <Accordion/>
+  },
+  {
+    path: "/app2",
+    name: "App 2",
+    component: <App2/>
+  }
+]
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        Creating Apps Collection
-      </header>
+      <Router>
+      <div className='inner-container'>
+          <section className="sidebar">
+            <ul>
+              {
+                routes.map((route, index) => (
+                  <li key={index}>
+                    <Link to={route.path}>{route.name}</Link>
+                  </li>
+                ))
+              }
+            </ul>
+          </section>
+          <section className='content'>
+            <Switch>
+                {
+                  routes.map((route, index) => (
+                    <Route exact={route.exact} key={index} path={route.path} children={route.component}/>
+                  ))
+                }
+              </Switch>
+          </section>
+        </div>
+      </Router>
     </div>
   );
 }
---
+++ src/components/accordion/Accordion.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Accordion = () => {
+    return <h2>Accordion</h2>
+}
+
+export default Accordion;
