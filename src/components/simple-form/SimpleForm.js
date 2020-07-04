f1976ffab42b2c9717c095d2b8beb33bc4ac8b28
Maurikzio <m.morocho1196@gmail.com>
2020-07-04 04:49:46 +0300

    [ADD] SImple Form design and errors styles

--- src/components/simple-form/SimpleForm.js
+++ src/components/simple-form/SimpleForm.js
@@ -4,7 +4,33 @@ import './simple-form-styles.css';
 import img from './indian.png'
 
 const SimpleForm = () => {
-    const [ info, setInfo ] = useState({})
+    const [ info, setInfo ] = useState({firstName: '', lastName: '', email: '', phone: ''})
+    const [ errors, setErrors ] = useState({firstName: false, lastName: false})
+
+    const handleOnChange = e => {
+        const { target: { value, name } } = e;
+        setInfo({
+            ...info, [name]: value
+        })
+    }
+
+    const handleOnSubmit = e => {
+        e.preventDefault();
+        const { firstName, lastName, email, phone} = info;
+
+        setErrors({
+            ...errors, firstName: firstName === '', lastName: lastName === '',
+        })
+
+        const data = {
+            firstName, 
+            lastName, 
+            email, 
+            phone
+        }
+        // console.log(data);
+        console.log(errors);
+    }
 
     return(
         <div>
@@ -13,17 +39,53 @@ const SimpleForm = () => {
             </header>
             <div className='form-wrapper'>
                 <img src={img} className='image'/>
-                <form>
+                <form autoComplete='off' onSubmit={handleOnSubmit}>
                     <div className='first-name'>
                         <p><strong>First name:</strong></p>
                         <p>
                             <input
                                 name='firstName'
-                                value={info.firstName && ''}
+                                type='text'
+                                value={info.firstName}
+                                onChange={handleOnChange}
+                                className={errors.firstName ? 'error' : ''}
                             />
                         </p>    
-
                     </div>
+                    <div className='last-name'>
+                        <p><strong>Last name:</strong></p>
+                        <p>
+                            <input
+                                name='lastName'
+                                type='text'
+                                value={info.lastName}
+                                onChange={handleOnChange}
+                            />
+                        </p>    
+                    </div>
+                    <div className='email'>
+                        <p><strong>Email:</strong></p>
+                        <p>
+                            <input
+                                name='email'
+                                type='emails'
+                                value={info.email}
+                                onChange={handleOnChange}
+                            />
+                        </p>    
+                    </div>
+                    <div className='phone'>
+                        <p><strong>Phone:</strong></p>
+                        <p>
+                            <input
+                                name='phone'
+                                type='tel'
+                                value={info.phone}
+                                onChange={handleOnChange}
+                            />
+                        </p>    
+                    </div>
+                    <button className='simple-form-btn'>Send Information</button>
                 </form>
             </div>
         </div>
